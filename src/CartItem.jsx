af856244f9be46e8007e38fba366f7c071abf06e
Can Serinyel <cserinyel@gmail.com>
2025-06-23 09:37:34 -0400

    CardItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,16 +9,25 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(item => {
+        total += parseFloat(item.cost.substring(1))*item.quantity
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
+  const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
 
 
   const handleIncrement = (item) => {
+    console.log(item)
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity+1}));
   };
 
   const handleDecrement = (item) => {
@@ -57,7 +66,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
